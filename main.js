f951f79874c274175efa8f26a1535ec8556935cb
iodonnell <ian.odonnell@temetra.com>
2017-01-19 13:59:09 +0000

    Store available sources better

--- main.js
+++ main.js
@@ -14,13 +14,8 @@ module.exports.loop = function () {
     for(let sourceNum in Memory.availableSources) {
         if(Memory.availableSources.hasOwnProperty(sourceNum)){
             let source=Memory.availableSources[sourceNum];
-            if (Memory.sources === undefined) {
-                Memory.sources={};
-            }
-            if (Memory.sources[source.id] === undefined ||
-                Memory.sources[source.id].dedicatedMiner === undefined) {
-                Memory.sources[source.id] = {};
-                Memory.sources[source.id].dedicatedMiner = 0;
+            if (source[source.id].dedicatedMiner === undefined) {
+                source[source.id].dedicatedMiner = 0;
             }
             if (Memory.sources && Memory.sources[source.id]){
                 console.log(Memory.sources[source.id]);
