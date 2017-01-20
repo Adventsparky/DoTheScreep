64810e9c587869c7d7fa2e3cc7bde98f8d8a0b10
Ian O'Donnell <info@ianodonnell.com>
2017-01-20 01:36:25 +0000

    more debug

--- data.js
+++ data.js
@@ -12,6 +12,8 @@ module.exports = {
     spawnInCreepRoom: function(creep) {
         for(let room_name in Memory.roomInfo){
             if(Memory.roomInfo.hasOwnProperty(room_name)) {
+                console.log(room_name);
+                console.log(creep.room.name);
                 if (room_name === creep.room.name) {
                     return Game.rooms[room_name].spawn;
                 }
