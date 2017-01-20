944b665bfc000cc9b9da6a16a9ffa91835fe504f
Ian O'Donnell <info@ianodonnell.com>
2017-01-20 01:38:08 +0000

    debug

--- data.js
+++ data.js
@@ -15,6 +15,9 @@ module.exports = {
                 console.log(room_name);
                 console.log(creep.room.name);
                 if (room_name == creep.room.name) {
+                    console.log('ok got the room');
+                    console.log(Game.rooms[room_name]);
+                    console.log(Game.rooms[room_name].spawn);
                     return Game.rooms[room_name].spawn;
                 }
             }
