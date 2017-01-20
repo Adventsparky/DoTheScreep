010fc1cff0ce2feddfc0103e689d473cb99b5876
Ian O'Donnell <info@ianodonnell.com>
2017-01-20 01:41:36 +0000

    Check if it's an object ref thing

--- data.js
+++ data.js
@@ -17,9 +17,9 @@ module.exports = {
                 if (room_name == creep.room.name) {
                     console.log('ok got the room');
                     console.log(Memory.roomInfo[room_name]);
-                    console.log(Memory.roomInfo[room_name].spawn);
-                    console.log(Memory.roomInfo[room_name].spawn.pos);
-                    return Memory.roomInfo[room_name].spawn;
+                    console.log(Memory.roomInfo[room_name].spawn[0]);
+                    console.log(Memory.roomInfo[room_name].spawn[0].pos);
+                    return Memory.roomInfo[room_name].spawn[0];
                 }
             }
         }
