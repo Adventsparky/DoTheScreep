992eb5e8ca04f3f28c3bf876c2d645434eca2d4d
Ian O'Donnell <info@ianodonnell.com>
2017-01-20 00:48:39 +0000

    Was storing some source info twice

--- main.js
+++ main.js
@@ -5,16 +5,15 @@ const manCave=Game.spawns.Bastion;
 module.exports.loop = function () {
 
     // Set up some lists of things we might use more than once per tick
-    Memory.availableSources=manCave.room.find(FIND_SOURCES);
+    let availableSources=manCave.room.find(FIND_SOURCES);
     Memory.structures=manCave.room.find(FIND_STRUCTURES);
 
     // RIP in pieces
     Tasks.clearMemoryOfDeadCreeples();
 
-    console.log(Memory.availableSources);
-    for(let sourceNum in Memory.availableSources) {
-        if(Memory.availableSources.hasOwnProperty(sourceNum)){
-            let source=Memory.availableSources[sourceNum];
+    for(let sourceNum in availableSources) {
+        if(availableSources.hasOwnProperty(sourceNum)){
+            let source=availableSources[sourceNum];
             if (source.dedicatedMiner === undefined) {
                 source.dedicatedMiner = 0;
             }
