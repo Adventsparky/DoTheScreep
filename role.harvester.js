d6249caf55405bf1d605d98bd0b7661ba0037133
Ian O'Donnell <info@ianodonnell.com>
2017-01-22 00:59:41 +0000

    forgot to clear the other target

--- role.harvester.js
+++ role.harvester.js
@@ -12,11 +12,14 @@ const roleHarvester = {
         if(!currentlyHarvesting && creep.carry.energy == 0) {
             // We haven't started harvesting yet and we're out of energy, creep's gotta eat
             creep.memory.targetSource = Tasks.findNearestEnergy(creep)
+            creep.memory.targetDropoff = false;
         }
 
         if(currentlyHarvesting && creep.carry.energy == creep.carryCapacity) {
             // We were harvesting and now we're full, time to dump
-            creep.memory.targetDropoff = Tasks.findBestEnergyDump(creep);
+             = Tasks.findBestEnergyDump(creep);
+            creep.memory.targetSource = false;
+
         }
 
         // Fallback for aimless creeps (like when this code went live, might be able to remove later)
