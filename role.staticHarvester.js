4a91cf0196170e9340308e89dec3d91952d44560
Ian O'Donnell <info@ianodonnell.com>
2017-01-22 00:09:35 +0000

    typo when picking source for collection

--- role.staticHarvester.js
+++ role.staticHarvester.js
@@ -19,6 +19,18 @@ const roleHarvester = {
             creep.memory.targetDropoff = Tasks.findBestEnergyDump(creep);
         }
 
+        // Fallback for aimless creeps (like when this code went live, might be able to remove later)
+        if(!creep.memory.targetSource && !creep.memory.targetDropoff) {
+            if(creep.carry.energy < creep.carryCapacity) {
+                // Find fresh source
+                creep.memory.targetSource = Tasks.findNearestEnergy(creep)
+            }
+            if(creep.carry.energy == creep.carryCapacity) {
+                // Find new drop off
+                creep.memory.targetDropoff = Tasks.findBestEnergyDump(creep);
+            }
+        }
+
         // Keep the setup checks above and these action perform checks separate, these actions need to happen every tick
         if(creep.memory.targetSource) {
             let targetSource = Game.getObjectById(creep.memory.targetSource);
@@ -27,9 +39,7 @@ const roleHarvester = {
                     creep.moveTo(targetSource);
                 }
             }
-        }
-
-        if(creep.memory.targetDropoff) {
+        } else if(creep.memory.targetDropoff) {
             let targetDropoff = Game.getObjectById(creep.memory.targetDropoff);
             // Let's make sure it's still a valid energy dump
             if(!Tasks.structureHasSpaceForEnergy(targetDropoff)) {
