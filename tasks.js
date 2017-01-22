7e32aff4885c56589680ebf517fafbb57e40baf1
Ian O'Donnell <info@ianodonnell.com>
2017-01-22 01:16:09 +0000

    debug

--- tasks.js
+++ tasks.js
@@ -87,7 +87,7 @@ module.exports = {
                     return {range: range, structure: structure}
                 },{range: 99999});
                 console.log('Chose '+JSON.stringify(target)+' for '+creep.name);
-                creep.memory.targetDropoff =  target.structure.id
+                return target.structure.id
             }catch(e) {
                 console.log(e);
             }
