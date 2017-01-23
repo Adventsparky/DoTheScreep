10e1c39825b752049589caf40d98ec5166e44833
iodonnell <ian.odonnell@temetra.com>
2017-01-23 11:26:24 +0000

    debug census

--- tasks.js
+++ tasks.js
@@ -240,6 +240,7 @@ module.exports = {
                         if (creepleCountForRole < role.minRoomPopulation) {
                             console.log('New: '+'need to spawn a ' + role.role + ' in '+roomId+', only have '+creepleCountForRole);
                             console.log(Game.rooms[roomId].energyCapacityAvailable);
+                            console.log(Memory.roleBuildCosts[role.role+'Stage2Parts']);
 
                             if(Game.rooms[roomId].energyCapacityAvailable > Memory.roleBuildCosts[role.role+'Stage2Parts']){
                                 console.log('Build big one');
