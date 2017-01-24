0f725c02fa4a0f1f4bfe86cb43ca7555e6e1f7bb
Ian O'Donnell <info@ianodonnell.com>
2017-01-24 00:12:11 +0000

    count which squares we checked

--- data.js
+++ data.js
@@ -66,7 +66,7 @@ module.exports = {
         for(let i=0; i<3; i++) {
             y=structure.pos.y - 1;
             for(let j=1; j<3; j++) {
-                // console.log('Check: '+x+','+y);
+                console.log('Check: '+x+','+y);
                 console.log(Game.map.getTerrainAt(x,y,structure.room.name));
                 if(Game.map.getTerrainAt(x,y,structure.roomName) == 'plain') {
                     spaces++;
