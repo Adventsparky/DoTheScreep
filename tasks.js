cceeb5f1623433865fc0a97ef6876b8c62533a63
Ian O'Donnell <info@ianodonnell.com>
2017-01-24 23:32:02 +0000

    Let's try spawn a big harvester

--- tasks.js
+++ tasks.js
@@ -290,6 +290,7 @@ module.exports = {
         return true;
     },
     checkIfWeAreReadyForStaticHarvesters : function(room) {
+        console.log(room);
         console.log(room.name);
         if(this.energyAvailableInRoom(Game.rooms[room.name]) > Memory.roleBuildCosts['staticHarvester']){
             // OK now we're onto something, lets check if we have enough regular creeps using an absolute minimum
