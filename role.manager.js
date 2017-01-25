43c3985e835c54bd4e5be8d2fe18f7e09fcfe3a9
Ian O'Donnell <info@ianodonnell.com>
2017-01-25 01:01:51 +0000

    typo in builder site search name

--- role.manager.js
+++ role.manager.js
@@ -31,7 +31,7 @@ module.exports = {
         parts: [WORK,CARRY,MOVE],
         stage2Parts: [WORK,WORK,CARRY,CARRY,MOVE,MOVE],
         role: 'upgrader',
-        targetRoomPopulation: 4,
+        targetRoomPopulation: 2,
         minRoomPopulation: 1,
         run: roleUpgrader.run
     }
