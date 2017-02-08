445526fb5aeb3a491ede9874e2a06ed57f5e9a59
iodonnell <ian.odonnell@temetra.com>
2017-02-08 15:04:16 +0000

    updated claimer to try fill controller with energy

--- role.basicClaimer.js
+++ role.basicClaimer.js
@@ -1,3 +1,6 @@
+const Tasks=require('tasks');
+const Query=require('data');
+
 const roleBasicClaimer = {
 
     /** @param {Creep} creep **/
@@ -7,14 +10,53 @@ const roleBasicClaimer = {
         if (claimFlag) {
 
             if (claimFlag.room == creep.room) {
+                let enemyData = Memory.roomInfo[creep.room.name].enemyData;
+                if (!enemyData) {
+                    // Fuck all to do, might as well retire
+                } else {
+                    let closestUnclaimedController = _.filter(enemyData.enemyStructures, function (structure) {
+                        if (structure.structureType == STRUCTURE_CONTROLLER) {
+                            return structure;
+                        }
+                    });
+
+                    if (closestUnclaimedController[0]) {
+                        creep.moveTo(closestUnclaimedController[0]);
+                        creep.claimController(closestUnclaimedController[0]);
+                    }
+                }
 
                 let controller = creep.room.find(FIND_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_CONTROLLER});
                 console.log(controller);
-                if (controller && controller[0] && controller[0].level == 0) {
-                    creep.moveTo(controller[0]);
-                    creep.claimController(controller[0]);
-                }
+                if (controller && controller[0]) {
+                    if (!controller[0].my && controller[0].level == 0) {
+                        creep.moveTo(controller[0]);
+                        creep.claimController(controller[0]);
+                    } else if (controller[0].my) {
 
+                        // Literally harvester code
+                        let currentlyHarvesting=creep.memory.targetSource || creep.memory.targetStorageSource;
+
+                        // Two checks to set up the harvesting flag only get run when it fills, or empties
+                        // Tasks.findNearestOrLeastBusySource(creep);
+
+                        if(!currentlyHarvesting && creep.carry.energy == 0) {
+                            // We haven't started harvesting yet and we're out of energy, creep's gotta eat
+                            Tasks.findNearestOrLeastBusySource(creep);
+                            delete creep.memory.targetDropoff;
+                        }
+
+                        if(currentlyHarvesting && creep.carry.energy == creep.carryCapacity) {
+                            // We were harvesting and now we're full, time to dump
+                            creep.memory.targetDropoff = controller[0];
+                            delete creep.memory.targetSource;
+                            delete creep.memory.targetStorageSource;
+                        }
+                    }
+
+                    Tasks.collectEnergy(creep);
+                    Tasks.depositEnergy(creep);
+                }
             } else {
                 creep.moveTo(claimFlag);
             }
