fa7f33a364351c21d4807f111e8191f9db7e4db4
Ian O'Donnell <info@ianodonnell.com>
2017-02-19 00:17:49 +0000

    Rework creep logic, trim the fat

--- manager.room.js
+++ manager.room.js
@@ -140,7 +140,7 @@ module.exports = {
                     && notEnoughStaticsInAction) {
                     // We need to check there's not one on the way to the source or one in the spawn Q
                     // Memory.highPrioritySpawns.push({'room':roomInfo.name,'role':'staticHarvester'});
-                    console.log(JSON.stringify(Tasks.spawnQueueEntry(roomInfo.name, roleName)));
+                    console.log(JSON.stringify(Tasks.spawnQueueEntry(roomInfo, roleName)));
                     // Tasks.addEntryToSpawnQueue(Tasks.spawnQueueEntry(roomInfo.name, roleName));
                 }
             }
