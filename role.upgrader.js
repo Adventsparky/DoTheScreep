5d642b79331b3c7ede01fd7f805ff152a894b688
Ian O'Donnell <info@ianodonnell.com>
2017-02-21 23:25:45 +0000

    debug

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,16 +1,22 @@
 const roleUpgrader = {
 
-    run: function(creep, room) {
+    run: function(creep, roomInfo) {
         // creep.say('u');
-        if(room.controller) {
+        if(roomInfo.controller) {
             if (creep.carry.energy == 0) {
                 creep.memory.upgrading = false;
-                creep.findNearestOrLeastBusySource(room);
+                creep.findNearestOrLeastBusySource(roomInfo);
             }
 
             if (!creep.currentlyUpgrading() && creep.carry.energy == creep.carryCapacity) {
                 creep.memory.upgrading = true;
                 delete creep.memory.targetSource;
+            }
+
+            // Aimless creeps who get their cycles broken particular when collecting or dumping
+            // energy and the target filled/expired/destroyed
+            if (creep.hasNoPurposeInLife()) {
+                creep.getABasicJob(roomInfo);
             }
 
             if (creep.currentlyUpgrading()) {
