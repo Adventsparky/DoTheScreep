253c18e83c21ff820183bbf98be0ba17e5baffaa
Ian O'Donnell <info@ianodonnell.com>
2017-02-25 09:57:41 +0000

    Some more default pos setup

--- prototype.structure.js
+++ prototype.structure.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Structure.prototype.structureHasSpaceForEnergy = function () {
-    if(this.structureType == STRUCTURE_CONTAINER) {
+    if(this.structureType == STRUCTURE_CONTAINER || this.structureType == STRUCTURE_STORAGE) {
         return _.sum(this.store) < this.storeCapacity;
     }
     return this.energy < this.energyCapacity;
