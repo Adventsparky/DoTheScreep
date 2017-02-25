bd3e550dd20b44785b6d98c62a603e140fedbe12
Ian O'Donnell <info@ianodonnell.com>
2017-02-25 09:20:34 +0000

    Some more default pos setup

--- tasks.js
+++ tasks.js
@@ -271,13 +271,13 @@ module.exports = {
 
                     if(roomInfo.mainSpawn.canCreateCreep(spawnRole.stage2Parts, creepName) == OK){
                         // console.log('Build big one');
-                        // spawnResult=roomInfo.mainSpawn.createCreep(spawnRole.stage2Parts, creepName, {role: spawnRole.role});
+                        spawnResult=roomInfo.mainSpawn.createCreep(spawnRole.stage2Parts, creepName, {role: spawnRole.role});
                     } else {
                         // console.log('Build little one');
-                        // spawnResult=roomInfo.mainSpawn.createCreep(spawnRole.parts, creepName, {role: spawnRole.role});
+                        spawnResult=roomInfo.mainSpawn.createCreep(spawnRole.parts, creepName, {role: spawnRole.role});
                     }
 
-                    if (spawnResult == OK) {
+                    if (spawnResult == creepName) {
                         // Remove from q
                         this.removeEntryFromSpawnQueue(roomInfo, spawnRole.role);
                     }
